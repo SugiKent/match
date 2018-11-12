41f840c892830378c3ec176b3bf55bf9157f721f
sugiken <ynmk1995@gmail.com>
2018-11-12 22:31:54 +0900

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,8 +63,8 @@ config.action_mailer.smtp_settings = {
  address:              'smtp.gmail.com',
  port:                  587,
  domain:               'gmail.com',
- user_name:            'yidongyi8@gmail.com', #gmailアドレス
- password:             'udit twjj ygmo iqiy',
+ user_name:            ENV['GMAIL_ADDRESS'], #gmailアドレス
+ password:             ENV['GMAIL_PASS'],
  authentication:       'plain',
  enable_starttls_auto:  true
 }
